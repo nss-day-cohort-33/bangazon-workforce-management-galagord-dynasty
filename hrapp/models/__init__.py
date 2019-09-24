9f80aca721174355ed5cfbf4929d7131819ae667
Shane Miller <shanemiller89@gmail.com>
2019-09-24 13:45:43 -0500

    added dummy data

--- hrapp/models/__init__.py
+++ hrapp/models/__init__.py
@@ -1,3 +1,5 @@
 from .employee import Employee
 from .computer import Computer
 from .employee_computer import EmployeeComputer
+from .training_program import TrainingProgram
+from .training_program_employee import TrainingProgramEmployee
