328f8a6823f079174bae347f5c00c13f3f8f29e2
Adam Knowles <acknowles524@gmail.com>
2019-09-25 19:17:41 -0500

    employees and departments are showing up in employee list

--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -1,6 +1,7 @@
 import sqlite3
 from django.shortcuts import render
 from hrapp.models import Employee
+from hrapp.models import Department
 from ..connection import Connection
 
 
@@ -17,8 +18,11 @@ def employee_list(request):
                 e.first_name,
                 e.last_name,
                 e.start_date,
-                e.is_supervisor
+                e.is_supervisor,
+                d.department,
+                e.department_id
             from hrapp_employee e
+            join hrapp_department d on e.department_id = d.id
             """)
 
             all_employees = []
@@ -31,13 +35,13 @@ def employee_list(request):
                 employee.last_name = row['last_name']
                 employee.start_date = row['start_date']
                 employee.is_supervisor = row['is_supervisor']
-                # employee.department = row['department']
+                employee.department_thing = row['department']
 
                 all_employees.append(employee)
 
-    template = 'employees/employees_list.html'
-    context = {
-        'employees': all_employees
-    }
+        template = 'employees/employees_list.html'
+        context = {
+            'all_employees': all_employees
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
