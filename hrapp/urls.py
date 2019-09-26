0de144b23dfa7fe233eafeb8860f3440cc4206ee
Misty DeRamus <mistyderamus@gmail.com>
2019-09-26 10:30:10 -0500

    update template

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import url, include
 from hrapp import views
 from django.contrib.auth import views as auth_views
 from .views import *
@@ -13,4 +13,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
     path('departments/', department_list, name='department_list'),
+    url(r'^departments/form$', form, name='department_form'),
 ]
