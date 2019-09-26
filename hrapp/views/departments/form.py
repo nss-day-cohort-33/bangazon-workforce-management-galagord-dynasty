01f4d9afa8303955390072950600e09b747cab23
Misty DeRamus <mistyderamus@gmail.com>
2019-09-25 20:50:41 -0500

    add buttons to department list

---
+++ hrapp/views/departments/form.py
@@ -0,0 +1,21 @@
+import sqlite3
+from django.shortcuts import render
+from ..connection import Connection
+from hrapp.models import Department
+from hrapp.models import Employee
+
+
+def get_department_info():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            d.id,
+            d.department,
+            d.budget
+        from hrapp_department d
+        """)
+
+        return db_cursor.fetchall()
